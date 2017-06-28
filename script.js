c1a1823ac302057d1cd29e80fcb25ba18fb5e94c
bananu7 <bananu7@o2.pl>
2017-06-28 18:45:30 +0200

    Initial

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    console.log('init');
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {        
+            console.log('signed in as ' + user.uid);
+
+            firebase.database().ref('things').on("child_added", function(data) {
+                console.log(data);
+                loadThingElem(data.val(), data.key);
+            }, function (errorObject) {
+                console.log("The read failed: " + errorObject.code);
+            });
+        } else {
+            firebase.auth().signInWithEmailAndPassword('example@example.com', 'example').catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log('auth error: ' + error.message)
+            });
+        }
+    });
+
+    $("#newThingAdd").on("click", addThing);
+});
+
+function addThing() {
+    var name = $("#newThingName").val();
+    console.log("adding", name)
+    firebase.database().ref('things').push(name);
+}
+
+function loadThingElem(name, id) {
+    var $thing = $('<div></div>');
+
+    $thing
+        .addClass('thing')
+        .text(name);
+
+    $('#container').append($thing);
+}
